4e5937474aef0080a1e6c61894fe0d51b4e14edc
Satyajit <slenka528@gmail.com>
2023-07-29 23:36:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let btnEl=document.querySelector("#btn")
+
+btnEl.addEventListener("mouseover",(event)=>{
+    const x =(event.pageX - btnEl.offsetLeft);
+    const y =(event.pagey - btnEl.offsetTop);
+    btnEl.style.setProperty("--xPos",x + "px");
+    btnEl.style.setProperty("--yPos",y + "px");
+
+});
